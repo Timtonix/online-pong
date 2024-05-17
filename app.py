bf553c11317d9d19f960a675b102320355956a1d
timtonix <timtonic@outlook.fr>
2024-05-17 17:55:34 +0200

    ask pseudo

--- app.py
+++ app.py
@@ -48,20 +48,46 @@ class Client:
     def recv_text(self):
         return self.sock.recv(1024).decode('utf-8')
 
+class Ecran:
+    def __init__(self, question):
+        self.question = question
+        self.ask = False
+        self.keys = {pyxel.KEY_A: "a", pyxel.KEY_B: "b", pyxel.KEY_C: "c", pyxel.KEY_D: "d", pyxel.KEY_E: "e", pyxel.KEY_F: "f", pyxel.KEY_G: "g", pyxel.KEY_H: "h", pyxel.KEY_I: "i", pyxel.KEY_J: "j", pyxel.KEY_K: "k", pyxel.KEY_L: "l", pyxel.KEY_M: "m", pyxel.KEY_N: "n", pyxel.KEY_O: "o", pyxel.KEY_P: "p", pyxel.KEY_Q: "q", pyxel.KEY_R: "r", pyxel.KEY_S:" s", pyxel.KEY_T: "t", pyxel.KEY_U: "u", pyxel.KEY_V: "v", pyxel.KEY_W:" w", pyxel.KEY_X: "x", pyxel.KEY_Y: "y", pyxel.KEY_Z: "z"}
+        self.answer = ""
+
+    def update(self):
+        if not self.ask:
+            return None
+        for key, value in self.keys.items():
+            if pyxel.btnp(key):
+                self.answer += value
+
+        if pyxel.btnp(pyxel.KEY_BACKSPACE):
+            self.ask = False
+
+
+    def draw(self):
+        if not self.ask:
+            return None
+        pyxel.text(0, 0, self.question, 2)
+        pyxel.text(0, 20, self.answer, 2)
 
 class App:
     def __init__(self):
         pyxel.init(128, 128, title="Pong", fps=60)
         pyxel.load("2.pyxres")
+        self.pseudo = Ecran("pseudo")
         self.client = Client("timtonix")
         self.client.connect()
         self.piece = Piece()
         pyxel.run(self.update, self.draw)
 
     def update(self):
+        self.pseudo.update()
         self.piece.update()
 
     def draw(self):
+        self.pseudo.draw()
         self.piece.draw()
 
 
